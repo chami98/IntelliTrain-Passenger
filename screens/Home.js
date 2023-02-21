c6f085cae9ec7d07dfae7e95c3b5dae674e7323d
chami98 <chamikara98@gmail.com>
2023-02-21 22:18:42 +0530

    Fetch user data via the API when user sign in

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-const Home = () => {
+const Home = ({ route }) => {
+
+    const { data } = route.params;
     return (
 
         <View style={styles.container}>
             <Text style={styles.text}>IntelliTrain</Text>
+            <Text style={styles.text}>Welcome {data.firstName}</Text>
+
             <Image
                 source={require('../assets/railway.png')}
             />
-
         </View>
     );
 }
