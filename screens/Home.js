c8a85dd32b8993f89d1823be6083048c5155968f
chami98 <chamikara98@gmail.com>
2023-02-22 01:17:07 +0530

    Changed the home screen design a bit

--- screens/Home.js
+++ screens/Home.js
@@ -7,12 +7,16 @@ const Home = ({ route }) => {
     return (
 
         <View style={styles.container}>
-            <Text style={styles.text}>IntelliTrain</Text>
-            <Text style={styles.text}>Welcome {data.firstName}</Text>
 
-            <Image
-                source={require('../assets/railway.png')}
-            />
+            <View style={styles.textContainer}>
+                <Text style={styles.appName}>IntelliTrain</Text>
+                <Text style={styles.slogan}>Taking your railway experience to the next level</Text>
+                <Image
+                    source={require('../assets/railway.png')}
+                    style={styles.image}
+                    resizeMode="contain"
+                /><Text style={styles.greeting}>Welcome, {data.firstName}!</Text>
+            </View>
         </View>
     );
 }
@@ -23,10 +27,40 @@ export default Home;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: '#f7f7f7',
     },
-    text: {
-        fontSize: 20
-    }
-})
+    image: {
+        width: '90%',
+        height: undefined,
+        aspectRatio: 1.5,
+        marginBottom: 24,
+    },
+    textContainer: {
+        alignItems: 'center',
+    },
+    appName: {
+        fontSize: 38,
+        fontWeight: 'bold',
+        marginBottom: -2,
+        color: '#3969b7'
+    },
+    slogan: {
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 16,
+        fontStyle: 'italic',
+    },
+    greeting: {
+        fontSize: 22,
+        backgroundColor: '#2ecc71',
+        borderWidth: 1,
+        borderColor: '#2ecc71',
+        borderRadius: 30,
+        padding: 15,
+        color: 'white',
+        fontWeight: 'bold',
+
+    },
+});
