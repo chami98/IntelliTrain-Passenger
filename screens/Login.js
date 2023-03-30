c30a3e4dbcfe40d3ef2cbb089c2ca6cb1d919326
chami98 <chamikara98@gmail.com>
2023-03-30 22:57:06 +0530

    Added Gif image to the login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text } from 'react-native'
+import { Alert, StyleSheet, Text, Image } from 'react-native'
 import React, { useState } from 'react'
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from '@firebase/app';
@@ -32,60 +32,55 @@ const Login = ({ navigation }) => {
     return (
         <NativeBaseProvider>
             <View style={styles.container}>
-                <Center w="100%">
-                    <Box safeArea p="2" py="8" w="90%" maxW="290">
-                        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-                            color: "warmGray.50"
-                        }}>
-                            Welcome
-                        </Heading>
-                        <Heading mt="1" _dark={{
-                            color: "warmGray.200"
-                        }} color="coolGray.600" fontWeight="medium" size="xs">
-                            Sign in to continue!
-                        </Heading>
+                <View style={styles.imageView}>
+                    <Image
+                        style={styles.image}
+                        source={require('../assets/login2.gif')
+                        }
+                    /></View>
+                <Box safeArea p="0" py="0" w="90%" maxW="290">
+                    <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+                        color: "warmGray.50"
+                    }}>
+                        Welcome
+                    </Heading>
+                    <Heading mt="1" _dark={{
+                        color: "warmGray.200"
+                    }} color="coolGray.600" fontWeight="medium" size="xs">
+                        Sign in to continue!
+                    </Heading>
 
-                        <VStack space={3} mt="5">
-                            <FormControl>
-                                <FormControl.Label>Email ID</FormControl.Label>
-                                <Input onChangeText={(text) => setEmail(text)} />
-                            </FormControl>
-                            <FormControl>
-                                <FormControl.Label>Password</FormControl.Label>
-                                <Input onChangeText={(text) => setPassword(text)} type="password" />
-                                <Link _text={{
-                                    fontSize: "xs",
-                                    fontWeight: "500",
-                                    color: "indigo.500"
-                                }} alignSelf="flex-end" mt="1">
-                                    Forget Password?
-                                </Link>
-                            </FormControl>
-                            <Button onPress={handleSignIn} mt="2" colorScheme="indigo">
-                                Sign in
-                            </Button>
-                            <HStack mt="6" justifyContent="center">
-                                <Text fontSize="sm" color="coolGray.600" _dark={{
-                                    color: "warmGray.200"
-                                }}>
-                                    I'm a new user.{" "}
-                                </Text>
-                                {/* <Link _text={{
-                                color: "indigo.500",
-                                fontWeight: "medium",
-                                fontSize: "sm"
-                            }} href="#"
-                                onPress={navigation.navigate('Signup')}
-                            >
-                                Sign Up
-                            </Link> */}
-                                <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-                                    <Text style={{ color: 'blue' }}>Sign Up</Text>
-                                </TouchableOpacity>
-                            </HStack>
-                        </VStack>
-                    </Box>
-                </Center>
+                    <VStack space={3} mt="5">
+                        <FormControl>
+                            <FormControl.Label>Email ID</FormControl.Label>
+                            <Input onChangeText={(text) => setEmail(text)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>Password</FormControl.Label>
+                            <Input onChangeText={(text) => setPassword(text)} type="password" />
+                            <Link _text={{
+                                fontSize: "xs",
+                                fontWeight: "500",
+                                color: "indigo.500"
+                            }} alignSelf="flex-end" mt="1">
+                                Forget Password?
+                            </Link>
+                        </FormControl>
+                        <Button onPress={handleSignIn} mt="2" colorScheme="indigo">
+                            Sign in
+                        </Button>
+                        <HStack mt="6" justifyContent="center">
+                            <Text fontSize="sm" color="coolGray.600" _dark={{
+                                color: "warmGray.200"
+                            }}>
+                                I'm a new user.{" "}
+                            </Text>
+                            <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+                                <Text style={{ color: 'blue' }}>Sign Up</Text>
+                            </TouchableOpacity>
+                        </HStack>
+                    </VStack>
+                </Box>
             </View>
         </NativeBaseProvider>
 
@@ -137,5 +132,12 @@ const styles = StyleSheet.create({
         color: '#0782f9',
         fontWeight: '700',
         fontSize: 16
-    }
+    }, image: {
+        height: 270,
+        width: 270,
+    }, imageView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: -35,
+    },
 })
